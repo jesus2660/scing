cb9faba34fed9f0d168d39d8685c61a5731ed9b5
Jesus Gomez <jesusgp22@gmail.com>
2015-05-05 12:16:22 -0430

    Agregados multiples links en la barra de navegacion, agregando stubs para la generacion de informes.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, url
 
-from main.views import index
+from main.views import stats_yearly
+from views import index, manual, docs, stats
 
 
 urlpatterns = patterns('',
-    url(r'^$',index)
+    url(r'^$',index),
+    url(r'^manual/$',manual),
+    url(r'^documentacion/$',docs),
+    url(r'^estadisticas/$',stats),
+    url(r'^estadisticas/anual/$',stats_yearly),
 )
--- main/views.py
+++ main/views.py
@@ -1,4 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from main.models import Proyecto
+
 
 def index(request):
+    """muestra la pagina principal"""
     return render(request,'index.html',{})
+
+def manual(request):
+    """muestra el manual de uso"""
+    return render(request,'manual.html',{})
+
+def docs(request):
+    """muestra la documentacion"""
+    return render(request,'docs.html',{})
+
+def stats(request):
+    """muestra el formulario para desplegar los distintos informes y 
+    estadisticas disponibles a partir de la informacion de la base de datos"""
+    return render(request,'stats.html',{})
+
+def stats_yearly(request):
+    """retira todos los proyectos activos (nuevos y renovados) durante el año
+    seleccionado y genera las tablas y estadisticas del informe de gestion anual"""
+    proyectos = Proyecto.objects.all().exclude(estatus="C")
+    return render(request,'yearly.html',{"proyectos":proyectos})
